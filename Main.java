2a8170184b91d6df84803171b6f1504576d1cb66
soren <soren.benson2@gmail.com>
2026-03-06 10:07:10 -0600

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {29, 10, 14, 37, 13};
+        int[] array = {29, 10, 14, 37, 13, 17, 90, 6, 1, 73};
         System.out.println("Original Array: ");
         printArray(array);
         
@@ -18,6 +18,18 @@ public class Main {
     }
 
     public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++){
+            int savedIndex = i;
+            for (int t = i+1; t < array.length; t++){
+                if (array[t] < array[savedIndex]){
+                    savedIndex = t;
+                    System.out.println("yes");
+                }
+            }
+            int tempValue=array[i];
+            array[i]=array[savedIndex];
+            array[savedIndex]=tempValue;
+        }
         
     }
 }
